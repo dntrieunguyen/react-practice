e47f678c98753a0c544fdae1eb3a55cc8ae5356c
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-22 16:12:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.scss';
-import Lab201 from './components/Lab201';
 
 function App() {
    return (
       <>
-         <Lab201></Lab201>
+         <h2>Hello guys</h2>
       </>
    );
 }
