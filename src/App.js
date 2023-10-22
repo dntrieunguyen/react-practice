1247ccf45bbdddbf486156ea551632400f9d230d
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-22 17:49:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Typography, Divider } from 'antd';
-import './App.css';
+import './App.scss';
 import TodoList from './components/TodoList';
 import Filters from './components/Filters';
 
@@ -8,6 +8,7 @@ const { Title } = Typography;
 function App() {
    return (
       <div
+         className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
          style={{
             width: 500,
             margin: '0 auto',
