afaf4abf42cde4f5faa1f7196633130f730cb72f
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-31 18:07:04 +0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.scss';
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+   const [cartIsShown, setCartIsShown] = useState(false);
+
+   const showCartHandler = () => {
+      setCartIsShown(true);
+   };
+
+   const hideCartHandler = () => {
+      setCartIsShown(false);
+   };
+
    return (
-      <>
-         <h2>Hello guy</h2>
-      </>
+      <CartProvider>
+         {cartIsShown && <Cart onClose={hideCartHandler} />}
+         <Header onShowCart={showCartHandler} />
+         <main>
+            <Meals />
+         </main>
+      </CartProvider>
    );
 }
 
