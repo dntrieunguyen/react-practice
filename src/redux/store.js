113a59984a6bd2f18bd324b808ad2dc993297f1f
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-31 09:44:12 +0700

    Completed Lab 15

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,4 @@
 import { legacy_createStore as createStore } from 'redux';
 import { rootReducer } from './reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const composeEnhencers = composeWithDevTools();
-
-export const store = createStore(rootReducer, composeEnhencers);
+export const store = createStore(rootReducer);
