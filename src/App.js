8a65b3614f0975551bf766ef3264e4612d97e5a4
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-06 09:59:37 +0700

    Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
+import Form from './components/Form';
+import MovieDisplay from './components/MovieDisplay';
+import './components/style.scss';
 
+// https://movies-lab-98948-default-rtdb.firebaseio.com/movie.json
 function App() {
+   const [data, setData] = useState([]);
+   const [fetchDataFlag, setFetchDataFlag] = useState(false);
+
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const res = await fetch(
+               'https://movies-lab-98948-default-rtdb.firebaseio.com/movie.json',
+            );
+            const data = await res.json();
+            setData(Object.values(data));
+         } catch (error) {
+            console.log(error);
+         }
+      };
+
+      if (fetchDataFlag) {
+         fetchData();
+      }
+   }, [fetchDataFlag]);
+
+   const handleOnClickFetchData = () => {
+      setFetchDataFlag(true);
+   };
+
    return (
       <>
-         <h2>Hello guy</h2>
+         <Form></Form>
+         <section>
+            <button onClick={handleOnClickFetchData}>Fetch Movie</button>
+         </section>
+         <MovieDisplay data={data}></MovieDisplay>
       </>
    );
 }
