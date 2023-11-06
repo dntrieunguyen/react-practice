1d9e4a8442519cf7d1486f7f123d087c7ed5ea22
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-06 10:53:02 +0700

    Completed

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
 
       if (fetchDataFlag) {
          fetchData();
+         setFetchDataFlag(false);
       }
    }, [fetchDataFlag]);
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form() {
+   const [formData, setFormData] = useState({
+      openingText: '',
+      releaseDate: '',
+      title: '',
+   });
+
+   const handleFormChange = e => {
+      const { name, value } = e.target;
+      setFormData(prev => ({ ...prev, [name]: value }));
+   };
+
+   const handleClickAddBtn = async () => {
+      //   console.log(formData);
+      const { openingText, releaseDate, title } = formData;
+      const moviePost = {
+         openingText,
+         releaseDate,
+         title,
+      };
+      //   console.log(moviePost);
+
+      try {
+         const res = await fetch(
+            'https://movies-lab-98948-default-rtdb.firebaseio.com/movie.json',
+            {
+               method: 'POST',
+               headers: {
+                  'Content-Type': 'application/json',
+               },
+               body: JSON.stringify(moviePost),
+            },
+         );
+         if (!res) {
+            console.log('Lỗi Gửi');
+         }
+      } catch (error) {
+         console.log(error);
+      }
+
+      setFormData({
+         openingText: '',
+         releaseDate: '',
+         title: '',
+      });
+   };
+
+   const { openingText, releaseDate, title } = formData;
+
    return (
       <>
          <section>
             <form className="control">
-               <label>Title</label>
-               <input type="text" />
+               <label name="title">Title</label>
+               <input
+                  name="title"
+                  onChange={handleFormChange}
+                  value={title}
+                  type="text"
+               />
 
-               <label>Opening Text</label>
-               <textarea type="text" />
+               <label name="openingText">Opening Text</label>
+               <textarea
+                  name="openingText"
+                  onChange={handleFormChange}
+                  value={openingText}
+                  type="text"
+               />
 
-               <label>Release Date</label>
-               <input type="text" />
+               <label name="releaseDate">Release Date</label>
+               <input
+                  name="releaseDate"
+                  onChange={handleFormChange}
+                  value={releaseDate}
+                  type="date"
+               />
             </form>
-            <button>Add Movie</button>
+            <button onClick={handleClickAddBtn}>Add Movie</button>
          </section>
       </>
    );
