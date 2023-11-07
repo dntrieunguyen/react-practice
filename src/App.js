e2e16fbb8dca4de18a0e90343aa654bf060b00f4
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-07 15:23:13 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './components/Layout/Header.scss';
 
 const App = () => {
-   return <div></div>;
+   return (
+      <>
+         <header className="header">
+            <div className="button">hello</div>
+            <div className="icon">icon</div>
+            <div className="badge">badge</div>
+         </header>
+         <div className="main-image">
+            <img src={require('./assets/meals.jpg')} alt="" />
+         </div>
+      </>
+   );
 };
 
 export default App;
