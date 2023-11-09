80b97b8c8cbbab40e680785b76ac3da11a13043a
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-09 09:30:06 +0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import './App.scss';
+import React, { Component } from 'react';
 
-function App() {
-   return (
-      <>
-         <h2>Hello</h2>
-      </>
-   );
+export default class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         count: 0,
+         name: '',
+         password: '',
+      };
+      this.user = {
+         name: '',
+         password: '',
+      };
+   }
+
+   onChangeInputText = e => {
+      const { name, value } = e.target;
+      this.setState(state => ({ ...state, [name]: value }));
+   };
+
+   onChangeUser = e => {
+      const { name, value } = e.target;
+      this.setUser(state => ({ ...state, [name]: value }));
+      console.log(e);
+   };
+
+   render() {
+      return (
+         <>
+            <form action="">
+               <div>
+                  <label htmlFor="" name="name">
+                     Name
+                  </label>
+                  <input
+                     type="text"
+                     name="name"
+                     value={this.state.name}
+                     onChange={this.onChangeInputText}
+                  />
+               </div>
+               <div>
+                  <label htmlFor="" name="password">
+                     password
+                  </label>
+                  <input
+                     type="password"
+                     name="password"
+                     value={this.state.password}
+                     onChange={this.onChangeInputText}
+                  />
+               </div>
+            </form>
+            {/* <div>
+               <label htmlFor="" name="count"></label>
+               <input
+                  name="count"
+                  onChange={e => this.onChangeInputText(e)}
+                  value={this.state.count}
+                  type="number"
+               />
+               <button onClick={this.onIncrease}>+</button>
+               <button onClick={this.onDecrease}>-</button>
+            </div> */}
+         </>
+      );
+   }
 }
-
-export default App;
---
+++ src/components/Count.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Count({ style }) {
+   return (
+      <div className="bg-red">
+         <input type="text" />
+         <button>+</button>
+         <button>-</button>
+      </div>
+   );
+}
+
+export default Count;
