246b97d1c7b9ed593f6ccad981f41bf893431b7c
dntrieunguyen <dntrieunguyen@gmail.com>
2023-11-14 22:19:15 +0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import './App.scss';
+import { useEffect } from 'react';
 
 function App() {
+   const [state, setState] = useState(false);
+   const [data, setData] = useState([]);
+
+   const handleChange = () => {
+      setState(!state);
+   };
+
+   // https://jsonplaceholder.typicode.com/todos/1
+   const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+   useEffect(() => {
+      const fetchData = async url => {
+         try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setData(data);
+         } catch (error) {
+            console.log(error);
+         }
+      };
+      fetchData(url);
+      console.log(data);
+   }, [url]);
+   useEffect(() => {
+      return () => {};
+   }, []);
+
+   console.log(state);
    return (
       <>
-         <h2>Hello</h2>
+         <button onClick={handleChange}>click</button>
       </>
    );
 }
